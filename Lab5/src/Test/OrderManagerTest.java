fe45fad711fa3d89e3e876318397152eecc7c82e
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2024-10-08 12:05:57 +0600

    Created first test case

--- Lab5/src/Test/OrderManagerTest.java
+++ Lab5/src/Test/OrderManagerTest.java
@@ -1,15 +1,19 @@
 package Test;
 
-import org.junit.Before;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import src.OrderManager;
 import src.Product;
 import src.ProductFactory;
-import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class OrderManagerTest {
+    public static void main(String[] args) {
 
         List<Product> products = new ArrayList<>();
         ProductFactory productFactory = new ProductFactory();
@@ -25,14 +29,15 @@ public class OrderManagerTest {
 
         OrderManager.sortProducts(products);
 
-        @Test
-        public void testProductTotalPriceCalculation() {
-            Product acerLaptop = ProductFactory.createProduct("electronics", "Acer Laptop", 10, 1000);
-            assertEquals(1150.0, acerLaptop.calculateTotalPrice(), 0.01);
 
-            Product tShirt = ProductFactory.createProduct("clothing", "T-shirt", 50, 20);
-            assertEquals(22.0, tShirt.calculateTotalPrice(), 0.01);
-        }
+    }
+    @Test
+    public void testProductTotalPriceCalculation() {
+        ProductFactory productFactory = new ProductFactory();
+        Product acerLaptop = productFactory.createProduct("electronics", "Acer Laptop", 10, 1000);
+        assertEquals(1150.0, acerLaptop.calculateTotalPrice(), 0.01);
 
-
+        Product tShirt = productFactory.createProduct("clothing", "T-shirt", 50, 20);
+        assertEquals(22.0, tShirt.calculateTotalPrice(), 0.01);
+    }
 }
