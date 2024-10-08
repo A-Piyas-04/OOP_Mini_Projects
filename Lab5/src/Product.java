d6e5a7ff83fa308424dd86f8796d4f78605ab4fe
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2024-10-08 10:54:30 +0600

    made constructor and getter methods

--- Lab5/src/Product.java
+++ Lab5/src/Product.java
@@ -2,6 +2,25 @@ abstract class Product {
     protected String name;
     protected int stockQuantity;
     protected double unitPrice;
-
     public abstract double calculateTotalPrice();
+
+
+    public Product(String name, int stockQuantity, double unitPrice) {
+        this.name = name;
+        this.stockQuantity = stockQuantity;
+        this.unitPrice = unitPrice;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
 }
