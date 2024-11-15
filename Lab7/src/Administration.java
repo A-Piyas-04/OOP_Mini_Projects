0808f43708c2caca366977ccab52deed13d6b7e4
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2024-11-15 20:24:02 +0600

    Implemented the interfaces

--- Lab7/src/Administration.java
+++ Lab7/src/Administration.java
@@ -1,2 +1,4 @@
 public interface Administration {
+    void modifyUserDetails(String userId, String newEmail, String newPassword);
+    void renameFile(String oldFileName, String newFileName);
 }
--- Lab7/src/RaedOnly.java
+++ Lab7/src/RaedOnly.java
@@ -1,2 +1,3 @@
 public interface RaedOnly {
+    void viewUserDetails();
 }
