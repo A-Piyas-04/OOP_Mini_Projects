78157ad19280c2246230b7d114cb5eaffd47fdf9
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2024-11-15 20:18:03 +0600

    Initiated User class

--- Lab7/src/User.java
+++ Lab7/src/User.java
@@ -1,2 +1,14 @@
 public abstract class User {
+    protected String userId;
+    protected String username;
+    protected String email;
+    protected String password;
+
+    public User(String userId, String username, String email, String password) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
 }
