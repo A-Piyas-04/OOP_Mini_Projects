8c5a85ebf8c49f1cb0fd9ec70891c959dd1a0718
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2024-11-15 20:48:54 +0600

    fixed some errors in user manager

--- Lab7/src/UserManagementSystem.java
+++ Lab7/src/UserManagementSystem.java
@@ -1,2 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserManagementSystem {
+    private static UserManagementSystem instance; // Singleton instance
+    private List<User> users; // Shared resource: list of users
+
+    private UserManagementSystem() {
+        users = new ArrayList<>();
+        // Load users from User.csv
+        loadUsersFromFile("User.csv");
+    }
+
+    public static UserManagementSystem getInstance() {
+        if (instance == null) {
+            synchronized (UserManagementSystem.class) { // Thread-safe initialization
+                if (instance == null) {
+                    instance = new UserManagementSystem();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void loadUsersFromFile(String filePath) {
+        try {
+            List<String> lines = FileHandler.readFile(filePath);
+            for (String line : lines) {
+                String[] fields = line.split(",");
+                String userId = fields[0];
+                String username = fields[1];
+                String email = fields[2];
+                String password = fields[3];
+                UserType userType = UserType.valueOf(fields[4]);
+                switch (userType) {
+                    case ADMIN -> users.add(new AdminUser(userId, username, email, password));
+                    case POWER -> users.add(new PowerUser(userId, username, email, password));
+                    case REGULAR -> users.add(new RegularUser(userId, username, email, password));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public User authenticate(String username, String password) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                user.authenticate(password);
+                return user;
+            }
+        }
+        throw new SecurityException("User not found or invalid credentials.");
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
 }
