c16df1dedc1435840b4e837d4204a1b3d2999aa9
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2025-02-18 11:02:26 +0600

    added abstract class for toppings

---
+++ Review Lab/src/Toppings.java
@@ -0,0 +1,17 @@
+abstract class Topping {
+    String name;
+    double price;
+
+    public Topping(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
--- Review Lab/src/Yogurt.java
+++ Review Lab/src/Yogurt.java
@@ -1,3 +1,16 @@
 abstract class Yogurt {
+    String name;
+    double pricePerScoop;
 
+    public Yogurt(String name, double pricePerScoop) {
+        this.name = name;
+        this.pricePerScoop = pricePerScoop;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public double getPricePerScoop() {
+        return pricePerScoop;
+    }
 }
